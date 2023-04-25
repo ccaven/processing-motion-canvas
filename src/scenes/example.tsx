df5507043e9cfee462b8cefa6b4a2b4733524906
xacer <chacecaven@outlook.com>
2023-04-24 20:16:08 -0400

    implement signal support for parameters

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -7,6 +7,7 @@ import { createKhanLogo } from '../processing-motion-canvas/examples';
 import khanLogoTextSvg from '../processing-motion-canvas/examples/khan_academy_logo.svg';
 import { Img } from '@motion-canvas/2d/lib/components';
 import { createRef } from '@motion-canvas/core/lib/utils';
+import { createSignal } from '@motion-canvas/core/lib/signals';
 
 export default makeScene2D(function* (view) {
     const g = createGraphics(view);
@@ -15,21 +16,23 @@ export default makeScene2D(function* (view) {
 
     const mainColor = color(108, 199, 162);
 
+    
+
     const logoRef = createKhanLogo(g, mainColor);
 
     let logoTextRef = createRef<Img>();
     
     yield view.add(<Img src={khanLogoTextSvg} ref={logoTextRef} scale={4.0} x={375}/>);
 
-    logoTextRef().opacity(0);
+    let t = createSignal(0);
+    
+    logoRef().position.x(() => -300 * t());
+    logoTextRef().position.x(() => 375 - 300 * t());
+    logoTextRef().opacity(() => t());
 
     yield* logoRef().scale(2.0, 1.0).to(1.0, 1.0);
 
-    yield* all(
-        logoRef().position.x(-300, 1.0),
-        logoTextRef().opacity(1, 1.0),
-        logoTextRef().position.x(75, 1.0)
-    );
+    yield* t(1.0, 1.0);
 
     yield* waitFor(1.0);
 });
